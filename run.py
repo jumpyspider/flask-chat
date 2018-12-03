57e04815035f03cbb911c3e6e9e7ac73e390d2c9
David Roberts <cezroberts@hotmail.com>
2018-12-03 13:17:59 +0000

    Completed

--- run.py
+++ run.py
@@ -1,30 +1,53 @@
 import os
-from flask import Flask, redirect
+from datetime import datetime
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
-messages = []
+
+
+def write_to_file(filename, data):
+    """Handle the process of writing data to a file"""
+    with open(filename, "a") as file:
+        file.writelines(data)
 
 
 def add_messages(username, message):
-    """Add messages to the `messages` list"""
-    messages.append("{}: {}".format(username, message))
+    """Add messages to the `messages` text file"""
+    write_to_file("data/messages.txt", "({0}) {1} - {2}\n".format(
+            datetime.now().strftime("%H:%M:%S"),
+            username.title(),
+            message))
 
 
 def get_all_messages():
-    """Get al of the messages and separate them by a `br`"""
-    return "<br>".join(messages)
+    """Get all of the messages and separate them by a `br`"""
+    messages = []
+    with open("data/messages.txt", "r") as chat_messages:
+        messages = chat_messages.readlines()
+    return messages
 
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
     """Main page with instructions"""
-    return "To send a message use /USERNAME/MESSAGE"
+    
+    # Handle POST request
+    if request.method == "POST":
+        write_to_file("data/users.txt", request.form["username"] + "\n")
+        return redirect(request.form["username"])
+    return render_template("index.html")
 
 
-@app.route('/<username>')
+@app.route('/<username>', methods=["GET", "POST"])
 def user(username):
     """Display chat messages"""
-    return "<h1>Welcome, {0}</h1> {1}".format(username, messages)
+    
+    if request.method == "POST":
+        add_messages(username, request.form["message"] + "\n")
+    
+    messages = get_all_messages()
+    return render_template("chat.html",
+                            username=username, chat_messages=messages)
 
 
 @app.route('/<username>/<message>')
